1f243825d21ef0356e34b33ca840a908fb5a6cc7
Shougo Matsushita <Shougo.Matsu@gmail.com>
2015-07-04 20:54:53 +0900

    Add deoplete source

---
+++ rplugin/python3/deoplete/sources/look.py
@@ -0,0 +1,52 @@
+#=============================================================================
+# FILE: look.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license  {{{
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to
+#     permit persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#
+#     The above copyright notice and this permission notice shall be included
+#     in all copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# }}}
+#=============================================================================
+
+import os
+import os.path
+import subprocess
+from .base import Base
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'look'
+        self.mark = '[look]'
+        self.min_pattern_length = 4
+        self.executable_look = self.vim.eval('executable("look")')
+        self.encoding = self.vim.eval('&encoding')
+
+    def gather_candidates(self, context):
+        if not self.executable_look:
+            return []
+
+        try:
+            words = subprocess.check_output(
+                    ['look', context['complete_str']]).splitlines()
+        except subprocess.CalledProcessError:
+            return []
+
+        return [{ 'word': x.decode(self.encoding) } for x in words]
+
